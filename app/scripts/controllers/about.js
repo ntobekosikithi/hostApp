7b7a4006f013e73cf0489030a19d7d9aa6dcab40
ntobekosikithi <ntobekosikithi@gmail.com>
2016-11-28 08:31:30 +0200

    'First'

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hostAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hostAppApp
+ */
+angular.module('hostAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hostAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hostAppApp
+ */
+angular.module('hostAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
